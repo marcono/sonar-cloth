54b0a9835e1092fe183d65b5a14188099693d78e
Marco Conoscenti <marco.conoscenti@polito.it>
2020-12-11 18:04:39 +0100

    add new version of cloth

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
-source makefile.variable
+# source makefile.variable
 
-folder=lib
-path=$ipath$folder
+# folder=lib
+# path=$ipath$folder
 
-if [ $# -lt 2 ]
-then
-    echo "usage: $0 <seed> <is-preprocess>"
-    exit
-fi
+# if [ $# -lt 1 ]
+# then
+#     echo "usage: $0 <path-results>"
+#     exit
+# fi
 
-LD_LIBRARY_PATH=$path GSL_RNG_SEED=$1  ./cloth $2
+#LD_LIBRARY_PATH=$path GSL_RNG_SEED=1992  ./cloth 
+GSL_RNG_SEED=1992  ./cloth
+
+python batch-means.py
+
+#cp  *.csv *.json log $1
